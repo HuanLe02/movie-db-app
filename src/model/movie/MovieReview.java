3e03a3def96740d18ffa393ed7d87178787c88eb
AI-bino <mendezgabriel2000@gmail.com>
2022-04-03 15:34:48 -0500

    Added framework for MovieReview.java

---
+++ src/model/movie/MovieReview.java
@@ -0,0 +1,8 @@
+package model.movie;
+
+public class MovieReview {
+    protected double userScore = 0;
+    protected String userName = "";
+    protected String userComment = "";
+    protected String imdbID = "";
+}
