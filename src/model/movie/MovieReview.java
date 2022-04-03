c411175f42dc390c0c8a0b9195b069ca73d55f4a
Huan Le <gawdymoney.98@gmail.com>
2022-04-03 15:42:13 -0500

    Change visibility of MovieReview fields

--- src/model/movie/MovieReview.java
+++ src/model/movie/MovieReview.java
@@ -1,8 +1,8 @@
 package model.movie;
 
 public class MovieReview {
-    protected double userScore;
-    protected String userName;
-    protected String userComment;
-    protected String imdbID;
+    private double userScore;
+    private String userName;
+    private String userComment;
+    private String imdbID;
 }
