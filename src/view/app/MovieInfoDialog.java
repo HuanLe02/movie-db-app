f7b51bbc767c877e4cbb786a64fc2e94a598820d
rkooi88 <rkooi@email.unc.edu>
2022-04-03 14:38:41 -0500

    Added MovieInfoDialog file

---
+++ src/view/app/MovieInfoDialog.java
@@ -0,0 +1,19 @@
+package view.app;
+
+import model.movie.Movie;
+
+import javax.swing.*;
+
+public class MovieInfoDialog extends JDialog {
+
+    // displayed fields
+
+    /**
+     * Constructor
+     * @param mov: movie to display
+     */
+    MovieInfoDialog(Movie mov) {
+
+    }
+
+}
